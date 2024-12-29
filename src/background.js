c9b7f2df89e1a2b3c88f5c99e0e8af781fc082e5
Ajordat <alexjortri@gmail.com>
2024-12-29 19:30:07 +0100

    Added collection extractor with progress bar

--- src/background.js
+++ src/background.js
@@ -6,6 +6,7 @@ browser.runtime.onInstalled.addListener(() => {
     console.log("Deck Importer Extension Installed!");
 });
 
+const MAX_ITEMS_PER_PAGE = 36;
 
 async function getNextAuthToken() {
     const response = await fetch("https://www.altered.gg/api/auth/session", { credentials: "include" });
@@ -77,7 +78,64 @@ function parseDecklist(decklist, name, actions) {
     }
 }
 
+async function fetchPartialCollection(itemsPerPage, pageIndex, accessToken) {
+
+    const response = await fetch(`https://api.altered.gg/cards/stats?collection=true&itemsPerPage=${itemsPerPage}&page=${pageIndex}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${accessToken}`,
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to extract partial collection at page ${pageIndex}.`);
+    }
+
+    return await response.json();
+}
+
+function collectionIterator(itemsPerPage, accessToken, port) {
+    let currentPage = 1;
+    let lastPage = 1;
+
+    return {
+        async *[Symbol.asyncIterator]() {
+            do {
+                const collectionPage = await fetchPartialCollection(itemsPerPage, currentPage, accessToken);
+
+                yield collectionPage["hydra:member"];
+
+                let totalItems = collectionPage["hydra:totalItems"];
+                lastPage = Math.ceil(totalItems / itemsPerPage);
+
+                port.postMessage({ step: "progress", value: (currentPage / lastPage) * 100 });
+
+                currentPage += 1;
+            } while (currentPage <= lastPage);
+        }
+    }
+}
+
+async function extractCollection(accessToken, port) {
+    const collectionPage = collectionIterator(MAX_ITEMS_PER_PAGE, accessToken, port);
+    const collection = {};
+
+    for await (const page of collectionPage) {
+        for (const card of page) {
+            collection[card["reference"]] = card["inMyCollection"];
+        }
+    }
+
+    return collection;
+}
+
+function formatCollection(collection) {
+    return Object.entries(collection).map(([reference, count]) => `${count} ${reference}`).join("\n");
+}
+
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("received message: " + message.action);
     if (message.action === "importDeck") {
 
         (async () => {
@@ -89,7 +147,6 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
                 await addCards(deck, token);
 
                 sendResponse({ success: true, url: `http://altered.gg/decks/${deck.id}` });
-                return;
 
             } catch (error) {
                 console.error("Error importing deck:", error);
@@ -100,4 +157,31 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
         return true;
     }
     return false;
+});
+
+browser.runtime.onConnect.addListener((port) => {
+    console.log("connected to popup: " + port.name);
+
+    port.onMessage.addListener((message) => {
+        if (message.action == "extractCollection") {
+            (async () => {
+                try {
+                    const token = await getNextAuthToken();
+                    let collection = await extractCollection(token, port);
+
+                    let text = formatCollection(collection);
+
+                    port.postMessage({ step: "response", success: true, collection: text });
+
+                } catch (error) {
+                    console.error("Error extracting collection:", error);
+                    port.postMessage({ step: "response", success: false, msg: error.message });
+                }
+            })();
+        }
+    })
+    // Handle disconnection
+    port.onDisconnect.addListener(() => {
+        console.log("Popup disconnected.");
+    });
 });
